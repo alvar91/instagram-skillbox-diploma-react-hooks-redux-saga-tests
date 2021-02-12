1b2ba0e92cce67a24f0de7594af55eb6a65bb776
alvar91 <aleksey91scorp@bk.ru>
2021-02-12 20:57:37 +0300

    docs: (description of the function for removing duplicates from a collection)

--- src/utils/removeDuplicatesById.js
+++ src/utils/removeDuplicatesById.js
@@ -1,3 +1,10 @@
+/**
+* Function for removing dulicates from the collection
+* @param {Array{}} state Original array of unique objects
+* @param {Array{}} payload Array of objects with possible duplicates
+* @return {Array{}} An updated array of unique objects
+*/
+
 export default function removeDublicatesById(state, payload) {
   const idsOfState = state.map((post) => post.id);
   return payload.filter(({ id }) => !idsOfState.includes(id));
